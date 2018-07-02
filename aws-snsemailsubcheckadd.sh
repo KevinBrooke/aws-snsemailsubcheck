9da8cdb6811630e7d19510ef3f3749148220ae55
Kevin Brooke <kbrooke@live.com.au>
2018-07-02 10:25:21 +0100

    Updates for the colours to work on OSx

--- aws-snsemailsubcheckadd.sh
+++ aws-snsemailsubcheckadd.sh
@@ -1,38 +1,55 @@
 # Verify SNS Subscriptions on all topics in all regions.
+# OSX handles colours differently so we sort that here.
+unamestr=`uname`
+if [[ "$unamestr" == 'Linux' ]] || [[ "$unamestr" == 'FreeBSD' ]]; then
+    cNone='\e[00m'
+    cRed='\e[31m'
+    cGreen='\e[32m'
+    cDGreen='\e[92m'
+    cDYellow='\e[93m'
+elif [[ "$unamestr" == 'Darwin' ]]; then
+    cNone='\033[00m'
+    cRed='\033[31m'
+    cGreen='\033[32m'
+    cDGreen='\033[92m'
+    cDYellow='\033[93m'
+fi
+
 while getopts "e:" opt; do
   case ${opt} in
     e ) email=$OPTARG;;
     \? )
-        echo -e "\e[93m\nInvalid option: $OPTARG\n" 1>&2
-        echo -e "\e[93mUsage: script -e <emailaddress>"
+        echo -e "${cDYellow}\nInvalid option: $OPTARG\n" 1>&2
+        echo -e "${cDYellow}Usage: script -e <emailaddress>"
         exit 0
         ;;
   esac
 done
 
 if [[ -z "$email" ]]; then
-    echo -e "\e[93m\nInvalid option: $OPTARG\n" 1>&2
-    echo -e "\e[93mUsage: script -e <emailaddress>"
+    echo -e "${cDYellow}\nInvalid option: $OPTARG\n" 1>&2
+    echo -e "${cDYellow}Usage: script -e <emailaddress>"
     exit 0
 fi
 
 for region in `aws ec2 describe-regions --output text | cut -f3`
 do
-    echo -e "\e[93mChecking SNS topics in $region"
+    echo -e "${cDYellow}Checking SNS topics in $region"
     topics=`aws sns list-topics --output text --query 'Topics[].{output:TopicArn}' --region $region`
     if [[ ${#topics} > 0 ]]; then
         for topic in $topics
         do
-            echo -e "\e[92m    SNS topic found: $topic" 
+            echo -e "${cDGreen}    SNS topic found: $topic" 
             subs=`aws sns list-subscriptions-by-topic --topic-arn $topic --output text --region $region`
             if [[ $subs = *"$email"* ]]; then
-                echo -e "\e[32m        $email is already subscribed"
+                echo -e "${cGreen}        $email is already subscribed"
             else 
-                echo -e "\e[31m        $email is not subscribed, adding" 
+                echo -e "${cRed}        $email is not subscribed, adding" 
                 aws sns subscribe --topic-arn $topic --protocol email --notification-endpoint $email --region $region > /dev/null
             fi
         done
     else 
-        echo -e "\e[32m    No SNS topics found"
+        echo -e "${cGreen}    No SNS topics found"
     fi
 done
+echo -e "${cNone}"
--- aws-snsemailsubconfirmationcheck.sh
+++ aws-snsemailsubconfirmationcheck.sh
@@ -1,12 +1,28 @@
 # Get the SNS email subscription status of all topics and emails.
+# OSX handles colours differently so we sort that here.
+unamestr=`uname`
+if [[ "$unamestr" == 'Linux' ]] || [[ "$unamestr" == 'FreeBSD' ]]; then
+    cNone='\e[00m'
+    cRed='\e[31m'
+    cGreen='\e[32m'
+    cDGreen='\e[92m'
+    cDYellow='\e[93m'
+elif [[ "$unamestr" == 'Darwin' ]]; then
+    cNone='\033[00m'
+    cRed='\033[31m'
+    cGreen='\033[32m'
+    cDGreen='\033[92m'
+    cDYellow='\033[93m'
+fi
+
 for region in `aws ec2 describe-regions --output text | cut -f3`
 do
-    echo -e "\e[93mChecking SNS topics in $region"
+    echo -e "${cDYellow}Checking SNS topics in $region"
     topics=`aws sns list-topics --output text --query 'Topics[].{output:TopicArn}' --region $region`
     if [[ ${#topics} > 0 ]]; then
         for topic in $topics
         do
-            echo -e "\e[92m    SNS topic found: $topic" 
+            echo -e "${cDGreen}    SNS topic found: $topic" 
             subs=`aws sns list-subscriptions-by-topic --topic-arn $topic --output text --region $region`
             if [[ ${#subs} > 0 ]]; then
                 aws sns list-subscriptions-by-topic --topic-arn $topic --output text --region $region | 
@@ -15,16 +31,17 @@ do
                     subname=`echo $sub | cut -d ' ' -f2`
                     substatus=`echo $sub | cut -d ' ' -f5`
                     if [[ "$substatus" = *"PendingConfirmation"* ]]; then
-                        echo -e "\e[31m        $subname has not accepted the invite"
+                        echo -e "${cRed}        $subname has not accepted the invite"
                     else 
-                        echo -e "\e[32m        $subname has accepted the invite" 
+                        echo -e "${cGreen}        $subname has accepted the invite" 
                     fi
                 done
             else 
-                echo -e "\e[31m        No subscriptions found"
+                echo -e "${cRed}        No subscriptions found"
             fi
         done
     else 
-        echo -e "\e[32m    No SNS topics found"
+        echo -e "${cGreen}    No SNS topics found"
     fi
 done
+echo -e "${cNone}"
